7d1ee36984736bf86a77da6c24e3c6b9ef9173ba
Zenox01 <zainimdad007@gmail.com>
2022-04-16 10:42:18 +0500

    Updates

---
+++ precache-manifest.0a6372cda08e7ff93ebb66081a914d57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc316ee8f3fe49967d92e682434cd8d",
+    "url": "/CountdownTimer/index.html"
+  },
+  {
+    "revision": "bf15ab44943bf31fdac3",
+    "url": "/CountdownTimer/static/css/main.983033a1.chunk.css"
+  },
+  {
+    "revision": "52e1a4c9a17593835491",
+    "url": "/CountdownTimer/static/js/2.87e88fda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CountdownTimer/static/js/2.87e88fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf15ab44943bf31fdac3",
+    "url": "/CountdownTimer/static/js/main.6afc5130.chunk.js"
+  },
+  {
+    "revision": "18358970e582b671bd6e",
+    "url": "/CountdownTimer/static/js/runtime-main.7721c815.js"
+  },
+  {
+    "revision": "624871e1b6172da8c089029daf0c632e",
+    "url": "/CountdownTimer/static/media/SevenSegment.624871e1.ttf"
+  },
+  {
+    "revision": "90b4e40a8403d815d2a42d942008eef5",
+    "url": "/CountdownTimer/static/media/masthead-bg.90b4e40a.mp4"
+  }
+]);
